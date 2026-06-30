5e3980a746489dd4c9c23b01f63087adb810fd3d
Mahnoor Arif <mahnoorarif306@gmail.com>
2026-06-30 23:43:47 +0500

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
  
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -11,6 +12,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 });
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
@@ -23,5 +26,6 @@ app.get('/test', (req, res) => {
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
    
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,6 +17,6 @@ const userSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-const user = mongoose.user('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
 export default User;
