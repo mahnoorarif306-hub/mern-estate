820d4d68efc45444c736028234bd3e9172560743
Mahnoor Arif <mahnoorarif306@gmail.com>
2026-07-18 21:42:03 +0500

    add redux toolkit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  server: {
+    proxy: {
+      '/api': 
+      {
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+      secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+});
